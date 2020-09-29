b231c3d7053087aa922a82615416c7aec8de2bce
Wyatt Jacob Herkamp <wherkamp@kingtux.me>
2020-09-29 19:52:49 -0400

    init 3

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -24,7 +24,7 @@ author = 'Wyatt J Herkamp'
 # The full version, including alpha/beta/rc tags
 release = '1.0-SNAPSHOT'
 
-
+master_doc = 'index'
 # -- General configuration ---------------------------------------------------
 
 # Add any Sphinx extension module names here, as strings. They can be
