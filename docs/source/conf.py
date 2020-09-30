19b869666b9c78c68fa230b7045a93ed9ff9d759
Wyatt Jacob Herkamp <wherkamp@kingtux.me>
2020-09-29 20:01:30 -0400

    init 5

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -16,7 +16,7 @@
 
 
 # -- Project information -----------------------------------------------------
-
+import sphinx_rtd_theme
 project = 'Kakara Core'
 copyright = '2020, Wyatt J Herkamp'
 author = 'Wyatt J Herkamp'
@@ -30,7 +30,7 @@ master_doc = 'index'
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
-extensions = ['recommonmark'
+extensions = ['recommonmark', 'sphinx_rtd_theme'
 ]
 
 # Add any paths that contain templates here, relative to this directory.
@@ -47,7 +47,7 @@ exclude_patterns = []
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = 'alabaster'
+html_theme = 'sphinx_rtd_theme'
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
